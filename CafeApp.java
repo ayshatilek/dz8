c1c03307da6414f78d6485347c6defe3fd10af16
ayshatilek <aishatilekla@gmail.com>
2026-03-17 22:54:37 +0500

    Add files via upload

---
+++ CafeApp.java
@@ -0,0 +1,155 @@
+interface Beverage {
+    String getDescription();
+    double cost();
+}
+
+class Espresso implements Beverage {
+    public String getDescription() {
+        return "Espresso";
+    }
+
+    public double cost() {
+        return 2.0;
+    }
+}
+
+class Tea implements Beverage {
+    public String getDescription() {
+        return "Tea";
+    }
+
+    public double cost() {
+        return 1.5;
+    }
+}
+
+class Latte implements Beverage {
+    public String getDescription() {
+        return "Latte";
+    }
+
+    public double cost() {
+        return 3.0;
+    }
+}
+
+class Mocha implements Beverage {
+    public String getDescription() {
+        return "Mocha";
+    }
+
+    public double cost() {
+        return 3.5;
+    }
+}
+
+abstract class BeverageDecorator implements Beverage {
+    protected Beverage beverage;
+
+    public BeverageDecorator(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    public String getDescription() {
+        return beverage.getDescription();
+    }
+
+    public double cost() {
+        return beverage.cost();
+    }
+}
+
+class Milk extends BeverageDecorator {
+    public Milk(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Milk";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.5;
+    }
+}
+
+class Sugar extends BeverageDecorator {
+    public Sugar(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Sugar";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.2;
+    }
+}
+
+class WhippedCream extends BeverageDecorator {
+    public WhippedCream(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Whipped Cream";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.7;
+    }
+}
+
+class Caramel extends BeverageDecorator {
+    public Caramel(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Caramel";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.6;
+    }
+}
+
+class Chocolate extends BeverageDecorator {
+    public Chocolate(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Chocolate";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.8;
+    }
+}
+
+public class CafeApp {
+    public static void main(String[] args) {
+
+        Beverage drink1 = new Espresso();
+        drink1 = new Milk(drink1);
+        drink1 = new Sugar(drink1);
+
+        System.out.println(drink1.getDescription() + " = $" + drink1.cost());
+
+        Beverage drink2 = new Latte();
+        drink2 = new Caramel(drink2);
+        drink2 = new WhippedCream(drink2);
+        drink2 = new Chocolate(drink2);
+
+        System.out.println(drink2.getDescription() + " = $" + drink2.cost());
+
+        Beverage drink3 = new Mocha();
+        drink3 = new Milk(drink3);
+        drink3 = new Milk(drink3);
+        drink3 = new Sugar(drink3);
+
+        System.out.println(drink3.getDescription() + " = $" + drink3.cost());
+    }
+}
